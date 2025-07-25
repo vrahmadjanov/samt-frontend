0915f5cebd5c0f7f4db39d36f5943a0b1e46c061
Vladislav Rakhmatdzhanov <vrahmadjanov@fazo.info>
2025-07-25 12:00:44 +0500

    refactoring: atoms

--- src/shared/components/atoms/Input.jsx
+++ src/shared/components/atoms/Input.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
-import { StyledInput, Label, Wrapper } from './Input.styled';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: ${({ theme }) => theme.spacing.md};
+`;
+
+const Label = styled.label`
+  font-size: ${({ theme }) => theme.font.base};
+  color: ${({ theme }) => theme.colors.label};
+  margin-bottom: ${({ theme }) => theme.spacing.sm};
+`;
+
+const StyledInput = styled.input`
+  padding: 10px 14px;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  border-radius: ${({ theme }) => theme.radii.md};
+  background-color: ${({ theme }) => theme.colors.background};
+  font-size: ${({ theme }) => theme.font.base};
+  transition: 0.2s border-color;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary};
+    outline: none;
+    background-color: ${({ theme }) => theme.colors.white};
+  }
+`;
 
 const Input = ({ label, ...props }) => (
   <Wrapper>
